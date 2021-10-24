63c0dc338a177f358150336b0c682463cefae832
かいもち <86639005+kaimochipage@users.noreply.github.com>
2021-10-24 19:17:56 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 "use strict"
 {
+  //はみ出し要素検出
   document.querySelectorAll('*').forEach(el => el.clientWidth > document.body.clientWidth ? console.log(el) : null);"use strict"
+
+  //スクロール幅検出
+  $(function() {
+    $(window).scroll(function() {
+      $(".scroll-block").each(function() {
+        var scroll = $(window).scrollTop();
+        var blockPosition = $(this).offset().top;
+        var windowHeihgt = $(window).height();
+        if (scroll > blockPosition - windowHeihgt + 300) {
+          $(this).addClass("blockIn");
+        }
+      });
+    });
+  });
 }
